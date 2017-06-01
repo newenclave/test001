5460d73bbb66d6adcef651834c5901bd0c7b0619
newenclave <pikrat.acid@gmail.com>
2017-06-01 15:04:20 +0300

    test

---
+++ client.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main_c( )
+{
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,115 @@
 #include <iostream>
+#include <memory>
+
 #include "boost/asio.hpp"
 
-int main( )
+namespace ba = boost::asio;
+namespace bs = boost::system;
+
+using error_code = bs::error_code;
+
+using read_cb = std::function<void ( const error_code &, std::size_t ) >;
+
+struct i_client: public std::enable_shared_from_this<i_client> {
+    virtual ~i_client( ) = default;
+    virtual void close( ) = 0;
+    virtual void async_read( read_cb ) = 0;
+};
+
+using accept_cb = std::function<void ( const error_code &,
+                                       std::shared_ptr<i_client>) >;
+
+struct i_accept: public std::enable_shared_from_this<i_accept> {
+    virtual ~i_accept( ) = default;
+    virtual void close( ) = 0;
+    virtual void async_accept( accept_cb ) = 0;
+};
+
+struct tcp_acceptor: public i_accept {
+
+    struct tcp_client: public i_client {
+
+        tcp_client( ba::io_service &ios )
+            :sock_(ios)
+        { }
+
+        void close( )
+        {
+
+        }
+
+        void async_read( read_cb )
+        {
+
+        }
+
+        std::weak_ptr<i_accept> parent_;
+        ba::ip::tcp::socket sock_;
+    };
+
+    tcp_acceptor( ba::io_service &ios )
+        :acc_(ios)
+    {
+        using ep = ba::ip::tcp::endpoint;
+        acc_.open( ba::ip::tcp::v4( ) );
+        acc_.bind( ep(ba::ip::address_v4( ), 44556) );
+        acc_.listen( 5 );
+    }
+
+    void close( )
+    {
+        acc_.close( );
+    }
+
+    void async_accept( accept_cb cb )
+    {
+        auto client = std::make_shared<tcp_client>(acc_.get_io_service( ));
+        std::weak_ptr<i_accept> weak_this(shared_from_this( ));
+
+        auto this_cb = [client, weak_this, cb]( const error_code &err ) {
+            if( !err ) {
+                auto lck = weak_this.lock( );
+                if( lck ) {
+                    cb( err, client );
+                    return;
+                }
+            }
+            cb(err, std::shared_ptr<i_client>( ) );
+        };
+
+        acc_.async_accept( client->sock_, this_cb );
+    }
+
+    ba::ip::tcp::endpoint ep_;
+    ba::ip::tcp::acceptor acc_;
+};
+
+int main_c( );
+int main_s( )
 {
+    try {
+        ba::io_service ios;
+        ba::io_service::work wrk(ios);
+
+        auto ta = std::make_shared<tcp_acceptor>(std::ref(ios));
+
+        ta->async_accept( [ta](const error_code &e, std::shared_ptr<i_client> c) {
+            std::cout << e.message( ) << " Accept!\n";
+            ta->close( );
+            ta->acc_.get_io_service( ).stop( );
+        } );
+
+        ios.run( );
+
+    } catch( const std::exception &ex ) {
+        std::cerr << "G Error: " << ex.what( ) << "\n";
+    }
+
     return 0;
+}
+
+
+int main( int argc, char **argv )
+{
+    return argc == 1 ? main_s( ) : main_c( );
 }
